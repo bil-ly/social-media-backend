152c6f42eb777442a840faf1f88806431bc4ff7a
abongile <abongilebilly3@gmail.com>
2025-05-18 12:13:58 +0200

    feat(main): Add post method

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException, status
 from pydantic import BaseModel
 from typing import Dict
-import psycopg
+import psycopg , time
 
 app = FastAPI()
 
 
 class Post(BaseModel):
-    title : str = "Welcome to my instagram buddy"
+    title : str
     content : str
+    published: bool = True
 
 
 
+while True:
+    try:
+        conn = psycopg.connect("dbname=fastapi_media_app user=postgres password=Abilly#99")
+        cursor = conn.cursor()
+        print("Success")
+        break
 
-try:
-    conn = psycopg.connect("dbname=fastapi_media_app user=postgres password=Abilly#99")
-    cursor = conn.cursor
-    print("Success")
+    except Exception as error:
+        print("Connection failed")
+        print(error)
+        time.sleep(3)
 
-except Exception as error:
-    print("Connection failed")
-    print(error)
+@app.get("/posts")
+def get_posts():
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return posts
 
-@app.get("/")
-async def root():
-    return {"message": "HEllo Billy"}
 
-@app.get("/name")
-def getNames():
-    return {"names": {"Abongile","Billy"}}
+@app.get("/posts/{id}")
+def get_post(id: int):
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id),))
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail=f"The post with id:{id} does not exist")
+    return {post}
 
-@app.post("/name")
-async def addName(payload:Post):
-    # Covert the model to dict
-    post_dict = payload.dict()
-    return {f"Post with title {post_dict['title']} successfully added"}
-
+@app.post("/posts")
+def create_posts(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content,published) VALUES (%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return{new_post}
 
